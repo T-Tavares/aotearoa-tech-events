46e046a0ae3fb15e8829295f71eba7c1a622c676
T-Tavares <thiago.tavares.freire@gmail.com>
2024-10-21 18:55:17 +1300

    - Version one Layout done  - Horizontal Scrolling logic started ( need some refining )

--- src/app/Components/EventsCards/v2/EventsCards.tsx
+++ src/app/Components/EventsCards/v2/EventsCards.tsx
@@ -1,26 +1,95 @@
 'use client';
 
 import ss from './EventsCards.module.scss';
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef} from 'react';
 import type {Event} from '@/Types/Types';
 import {dataFetch} from './DataFetch';
+
+import {MdKeyboardArrowLeft, MdKeyboardArrowRight} from 'react-icons/md';
 
 import Card from '@/components/Card/v3/Card';
 import Loading from '@/components/Loading/Loading';
 
+interface ScrollCoordinates {
+    prevPosition: number;
+    currPosition: number;
+    offset: number;
+}
+
 const EventsCards: React.FC = () => {
     const [database, setDatabase] = useState<Event[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
+
+    const cardContainerRef = useRef<HTMLDivElement>(null);
+    const [isDragging, setIsDragging] = useState<boolean>(false);
+    const [scrollCoordinates, setScrollCoordinates] = useState<ScrollCoordinates>({
+        prevPosition: 0,
+        currPosition: 0,
+        offset: 0,
+    });
+
+    const handleMouseDown = (e: React.MouseEvent) => {
+        setIsDragging(true);
+        setScrollCoordinates({
+            prevPosition: e.clientX,
+            currPosition: e.clientX,
+            offset: 0,
+        });
+        cardContainerRef.current!.style.cursor = 'grabbing';
+    };
+
+    const handleMouseMove = (e: React.MouseEvent) => {
+        if (!isDragging) return;
+
+        setScrollCoordinates(prevState => {
+            return {
+                prevPosition: prevState.currPosition,
+                currPosition: e.clientX,
+                offset: prevState.prevPosition - e.clientX,
+            };
+        });
+    };
+
+    const handleMouseUp = () => {
+        setIsDragging(false);
+        cardContainerRef.current!.style.cursor = 'grab';
+    };
+    const handleMouseLeave = () => {
+        setIsDragging(false);
+        cardContainerRef.current!.style.cursor = 'grab';
+    };
+
+    useEffect(() => {
+        if (!isDragging) return;
+        const clientOnePercent = window.innerWidth / 100;
+        const scrollStep = scrollCoordinates.offset > 0 ? clientOnePercent : -clientOnePercent;
+        cardContainerRef.current!.scrollLeft += scrollStep;
+    }, [scrollCoordinates]);
+    // ------------------------------------------------------ //
 
     useEffect(() => {
         dataFetch({setDatabase, setLoading});
     }, []);
 
     return (
-        <div className={ss.eventsCardsContainer}>
-            {loading && <Loading message="We are searching for the Best Events, this wont take long" />}
-            {database && database.map(event => <Card event={event} key={event.title} />)}
-        </div>
+        <>
+            <div className={ss.month}>
+                <MdKeyboardArrowLeft />
+                <p>October</p>
+                <MdKeyboardArrowRight />
+            </div>
+            <div
+                className={ss.eventsCardsContainer}
+                ref={cardContainerRef}
+                onMouseDown={handleMouseDown}
+                onMouseMove={handleMouseMove}
+                onMouseUp={handleMouseUp}
+                onMouseLeave={handleMouseLeave}
+            >
+                {loading && <Loading message="We are searching for the Best Events, this wont take long" />}
+                {database && database.map(event => <Card event={event} key={event.title} />)}
+            </div>
+        </>
     );
 };
 
--- src/app/LayoutComponents/Content/v2/Content.tsx
+++ src/app/LayoutComponents/Content/v2/Content.tsx
@@ -2,30 +2,11 @@
 
 import ss from './Content.module.scss';
 import EventsCards from '@/Components/EventsCards/v2/EventsCards';
-// import Card from '@/Components/Card/v3/Card';
-
-// const CardData = {
-//     date: 'Wed 30th Oct, 5pm',
-//     region: 'Aotearoa New Zealand',
-//     title: 'Cryptocurrency NZ Meetup',
-//     blurb: "Join one of Cryptocurrency NZ's 12 crypto Meetups across Aotearoa New Zealand; where you can find other Kiwis active in the currency wars of the 21st century: Wellington, Palmerston North, Invercargill, Hamilton, Rotorua, Dunedin, Hawke's Bay, Nelson, Auckland, Christchurch, Tauranga, Queenstown.",
-//     type: 'In Person',
-//     organiserDetails: 'Cryptocurrency NZ Meetup',
-//     venue: 'Multiple locations across Aotearoa New Zealand',
-//     ticketLink: 'https://cryptocurrency.org.nz/community/',
-//     notes: '',
-// };
 
 const Content: React.FC = () => {
     return (
         <div className={ss.content}>
-            <h1>Content</h1>
-            {/* <Card event={CardData} />
-            <Card event={CardData} />
-            <Card event={CardData} />
-            <Card event={CardData} />
-            <Card event={CardData} />
-            <Card event={CardData} /> */}
+            <h1>Events</h1>
             <EventsCards />
         </div>
     );
